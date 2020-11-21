3462b3def4a16fa93a70260c7958ef7da2091e1c
Mull Zhang <mullzhang@users.noreply.github.com>
2020-11-22 03:34:20 +0900

    fix(setup): fix options of setup function

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 
 
 def read_requirements():
+    """Parse requirements from requirements.txt."""
     reqs_path = os.path.join('.', 'requirements.txt')
     with open(reqs_path, 'r') as f:
         requirements = [line.rstrip() for line in f]
@@ -15,7 +16,7 @@ setup(
     description='Python library for simulation of quantum annealing',
     long_description='README.md',
     author='mullzhang',
-    install_requires=['numpy', 'dimod', 'qutip'],
+    install_requires=read_requirements(),
     url='https://github.com/mullzhang/qanneal',
     license=license,
     packages=find_packages(exclude=('tests', 'docs')),
